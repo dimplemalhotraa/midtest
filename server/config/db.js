b53dec85b339e59a7cc3be64c9eeb99a90d4114f
dimplemalhotraa <61724691+dimplemalhotraa@users.noreply.github.com>
2022-03-06 21:29:29 -0500

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://mid-webapp:<password>@cluster0.vo5gf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
---
+++ server/routes/books.js
@@ -0,0 +1,138 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+     let newbook = book();
+     res.render('books/details', {
+         title: 'Add a new Item',
+         books: newbook
+     }) 
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+     let bookToAdd = book({
+      _id: req.body.id,
+      Title: req.body.title,
+      Price: req.body.price,
+      Author: req.body.author,
+      Genre: req.body.genre,
+      Year: req.body.year
+     });
+     book.create(bookToAdd, (err, item) =>{
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+          console.log(item);
+          res.redirect('/books');
+      }
+  });
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+     let id = req.params.id;
+
+     book.findById(id, (err, bookToEdit) => {
+         if(err)
+         {
+             console.log(err);
+             res.end(err);
+         }
+         else
+         {
+             res.render('books/details', {
+                 title: 'Edit Item', 
+                 books: bookToEdit
+             })
+         }
+     });
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+     let id = req.params.id
+
+     book.findByIdAndUpdate(req.params.id, {
+      Title: req.body.title,
+      Price: req.body.price,
+      Author: req.body.author,
+      Genre: req.body.genre,
+      Year: req.body.year
+      }, { new: true }, (err, data) => {
+      
+      if (!err) {
+      res.redirect('/books');
+      } else {
+      res.end(err);
+      }
+      })
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+     let id = req.dimple.id;
+
+     book.remove({_id: id}, (err) => {
+         if(err)
+         {
+             console.log(err);
+             res.end(err);
+         }
+         else
+         {
+             res.redirect('/books');
+         }
+     });
+});
+
+
+module.exports = router;
